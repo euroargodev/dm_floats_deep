6e31b1a4c162154cfd8a47a5793093f54b5dd70b
cabanesc <38859979+cabanesc@users.noreply.github.com>
2026-09-10 10:05:23 +0200

    update comments

--- src/MAIN_template_deep.m
+++ src/MAIN_template_deep.m
@@ -20,21 +20,28 @@ addpath('../lib/')
 
 flt_name = 6902882;
 
-% 1. Cpcor correction is applied in intermediate netcdf files (PSAL_ADJUSTED)
+% 1. Determine the Cpcor Correction
+%    if reference CTD are available (e.g launch CTD, nearby CTD) you can compute an optimum Cpcor_new value
+%    see : https://github.com/ArgoDMQC/Deep_Argo_DMTools/tree/master/DM_CPCor
+%    In delayed-mode, the optimized CPcor_new value should be used whenever it is available and is checked to be robust.
+%    Otherwise, the recommended CPcor_new value (-11.7e-8) should be used.
+%    see Argo DMQC Manual, section 3.10 for more details.
+
+% 2. Cpcor correction is applied in intermediate netcdf files (PSAL_ADJUSTED)
 % These intermediate files are not intended to be distributed on the gdac.
 % Ok for the Deep Arvor float. Requires modification for floats that do not auto-correct pressure.
 addpath(genpath('./correct_cpcor/'))
 
-corr_cpcor_in_netcdf(flt_name,'NEW_CPCOR',-13.58e-8);
+corr_cpcor_in_netcdf(flt_name,'NEW_CPCOR',-11.7e-8);
 
-% 2. Creates the OWC source file using adjusted PARAM
+% 3. Creates the OWC source file using adjusted PARAM
 addpath(genpath('./ow_source/'))
 
 create_float_source(flt_name,'force','adjusted');
 
-% 3. RUN OWC and obtain calibration files (see https://github.com/ArgoDMQC/matlab_owc)
+% 4. RUN OWC and obtain calibration files (see https://github.com/ArgoDMQC/matlab_owc)
 
-% 4. Creates final DMQC files with CpCOR adjustement, OWC correction if any and final Calibration/History sections.
+% 5. Creates final DMQC files with CpCOR adjustement, OWC correction if any and final Calibration/History sections.
 
 addpath(genpath('./create_dm_files/'))
 
